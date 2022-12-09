44d52d2164abb91c0725c7e4af420495f171191d
k.aras <25200089@tokeninc.com>
2022-12-09 17:47:58 +0300

    Metric Fix

--- implementations/ssd_models/custom_architecture/loss_function/custom_metric.py
+++ implementations/ssd_models/custom_architecture/loss_function/custom_metric.py
@@ -2,21 +2,16 @@ import tensorflow as tf
 
 
 def custom_mse(y_true, y_pred):
-    n_boxes = y_pred.shape[1]
+    
+    batch_size = tf.shape(y_pred)[0]
+    n_boxes = tf.shape(y_pred)[1]
 
-    positives = tf.cast(tf.reduce_max(y_true[:, :, 1:-10], axis=-1), dtype=tf.float32)
-    n_positive = tf.reduce_sum(positives)
+    positives = tf.cast(tf.reduce_max(y_true[:, :, 1:-10], axis=-1), dtype=tf.float64)
+    n_positive = tf.cast(tf.math.count_nonzero(positives), dtype=tf.float64)
 
-    y_pred[:, :, [-10, -8, -6, -4]] += tf.expand_dims(y_pred[:, :, -2], axis=-1)
-    y_pred[:, :, [-9, -7, -5, -3]] += tf.expand_dims(y_pred[:, :, -1], axis=-1)
-
-    y_true[:, :, [-10, -8, -6, -4]] += tf.expand_dims(y_true[:, :, -2], axis=-1)
-    y_true[:, :, [-9, -7, -5, -3]] += tf.expand_dims(y_true[:, :, -1], axis=-1)
-
-    mse_per_center = tf.cast(tf.reduce_mean((y_true[:, :, -10:-2] - y_pred[:, :, -10:-2])**2, axis=-1), dtype=tf.float32)
-
-    assert mse_per_center.shape[0] == n_boxes
-
-    mse_per_batch = tf.reduce_sum(positives * mse_per_center) / tf.cast(n_positive, dtype=tf.float32)
+    mse_per_center = tf.cast(tf.reduce_mean((y_true[:, :, -10:-2] - y_pred[:, :, -10:-2])**2, axis=-1)**2, dtype=tf.float64)
+    
+    mse_per_batch = tf.reduce_sum(mse_per_center * positives) / tf.cast(n_positive, dtype=tf.float64)
+    mse_per_batch = mse_per_batch * tf.cast(batch_size, dtype=tf.float64)
 
     return mse_per_batch
