cb4df3062157b67878c997f12cca6e7dc9b43754
k.aras <25200089@tokeninc.com>
2022-12-12 17:26:31 +0300

    12. December

--- implementations/data_generation/ssd_data_generator.py
+++ implementations/data_generation/ssd_data_generator.py
@@ -165,7 +165,7 @@ class SSD_Data_Generator:
         self.prelabels_list = []
 
     def get_ic_scale(self, ratio):
-        new_ratio = random.uniform(ratio - 0.05, ratio + 0.05)
+        new_ratio = random.uniform(ratio - 0.1, ratio + 0.1)
         org_size = self.background_ref.size[0]
         size = int(np.random.uniform(org_size * new_ratio, org_size * new_ratio))
         return tuple((size, size))
