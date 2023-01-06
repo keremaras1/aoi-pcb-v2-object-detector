b67fc02117c58e2e8d849b83cffe997c40ac321a
k.aras <25200089@tokeninc.com>
2023-01-06 17:23:40 +0300

    Custom Metric for Classification

--- implementations/ssd_models/custom_architecture/loss_function/custom_metric.py
+++ implementations/ssd_models/custom_architecture/loss_function/custom_metric.py
@@ -17,9 +17,38 @@ def custom_mse(y_true, y_pred):
     return mse_per_batch
 
 def class_mAP(y_true, y_pred):
-    class_true = tf.argmax(y_true[:, :, :-10], axis=-1)
-    class_pred = tf.argmax(y_pred[:, :, :-10], axis=-1)
     
-    average_precisions = tf.map_fn(lambda c: tf.metrics.average_precision_at_k(class_true, class_pred, k=c)[0], tf.range(2), dtype=tf.float64)
+    n_classes = tf.shape(y_true[:, :, :-10])[-1]
     
-    return tf.reduce_mean(average_precisions)
+    class_true = tf.cast(tf.argmax(y_true[:, :, :-10], axis=-1), dtype=tf.float32)
+    class_pred = tf.cast(tf.argmax(y_pred[:, :, :-10], axis=-1), dtype=tf.float32)
+    
+    def average_precision(c):
+        non_negative_true = tf.equal(class_true, c)
+        non_negative_pred = tf.equal(class_pred, c)
+        
+        negative_true = tf.not_equal(class_true, c)
+        negative_pred = tf.not_equal(class_pred, c)
+        
+        tp = tf.cast(tf.logical_and(non_negative_true, non_negative_pred), dtype=tf.float32)
+        fp = tf.cast(tf.logical_and(negative_true, non_negative_pred), dtype=tf.float32)
+        fn = tf.cast(tf.logical_and(non_negative_true, negative_pred), dtype=tf.float32)
+        
+        n_tp = tf.cast(tf.reduce_sum(tp), dtype=tf.float32)
+        n_fp = tf.cast(tf.reduce_sum(fp), dtype=tf.float32)
+        n_fn = tf.cast(tf.reduce_sum(fn), dtype=tf.float32)
+        
+        precision = n_tp / (n_tp + n_fp)
+        recall = n_tp / (n_tp + n_fn)
+        
+        average_p = precision * recall
+        
+        is_nan = tf.math.is_nan(average_p)
+        
+        final = tf.where(is_nan, tf.zeros_like(average_p), average_p)
+        
+        return final
+    
+    ap = tf.map_fn(average_precision, tf.range(1, n_classes, dtype=tf.float32), dtype=tf.float32)
+    
+    return tf.reduce_mean(ap)
