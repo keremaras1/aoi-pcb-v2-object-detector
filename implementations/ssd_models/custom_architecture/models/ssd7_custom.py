6c4ab5c3738f6d43675adcfec23115e2887314d1
k.aras <25200089@tokeninc.com>
2023-01-24 17:45:16 +0300

    From ELU to ReLU + Optimization

--- implementations/ssd_models/custom_architecture/models/ssd7_custom.py
+++ implementations/ssd_models/custom_architecture/models/ssd7_custom.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Lambda, Conv2D, MaxPooling2D, BatchNormalization, ELU, Reshape, Concatenate, Activation, GaussianNoise
+from keras.layers import Input, Lambda, Conv2D, MaxPooling2D, BatchNormalization, ReLU, ELU, Reshape, Concatenate, Activation, GaussianNoise
 from keras.regularizers import l2
 import keras.backend as K
 
@@ -64,26 +64,26 @@ def build_model(image_size,
                    kernel_regularizer=l2(l2_reg), name='conv1')(x1)
     conv1 = BatchNormalization(axis=3, momentum=0.99, name='bn1')(
         conv1)  # Tensorflow uses filter format [filter_height, filter_width, in_channels, out_channels], hence axis = 3
-    conv1 = ELU(name='elu1')(conv1)
+    conv1 = ReLU(name='relu1')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2), name='pool1')(conv1)
 
     conv2 = Conv2D(64, (5, 5), strides=(1, 1), padding="same", kernel_initializer='he_normal',
                    kernel_regularizer=l2(l2_reg), name='conv2')(pool1)
     conv2 = BatchNormalization(axis=3, momentum=0.99, name='bn2')(conv2)
-    conv2 = ELU(name='elu2')(conv2)
+    conv2 = ReLU(name='relu2')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2), name='pool2')(conv2)
 
     conv3 = Conv2D(128, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal',
                    kernel_regularizer=l2(l2_reg), name='conv3')(pool2)
     conv3 = BatchNormalization(axis=3, momentum=0.99, name='bn3')(conv3)
-    conv3 = ELU(name='elu3')(conv3)
+    conv3 = ReLU(name='relu3')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2), name='pool3')(conv3)
 
     conv4 = Conv2D(256, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal',
                    kernel_regularizer=l2(l2_reg), name='conv4')(pool3)
     
     conv4 = BatchNormalization(axis=3, momentum=0.99, name='bn4')(conv4)
-    conv4 = ELU(name='elu4')(conv4)
+    conv4 = ReLU(name='relu4')(conv4)
     
     ######### ADDITIONAL BASE NETWORK LAYERS ###########################
     
@@ -91,12 +91,12 @@ def build_model(image_size,
 
     conv5 = Conv2D(512, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='conv5')(pool4)
     conv5 = BatchNormalization(axis=3, momentum=0.99, name='bn5')(conv5)
-    conv5 = ELU(name='elu5')(conv5)
+    conv5 = ReLU(name='relu5')(conv5)
     pool5 = MaxPooling2D(pool_size=(2, 2), name='pool5')(conv5)
 
     conv6 = Conv2D(512, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='conv6')(pool5)
     conv6 = BatchNormalization(axis=3, momentum=0.99, name='bn6')(conv6)
-    conv6 = ELU(name='elu6')(conv6)
+    conv6 = ReLU(name='relu6')(conv6)
     #pool6 = MaxPooling2D(pool_size=(2, 2), name='pool6')(conv6)
 
     #conv7 = Conv2D(32, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='conv7')(pool6)
